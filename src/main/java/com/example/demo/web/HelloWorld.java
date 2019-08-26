6267cd7d258753bb6a6c4b816a6cee6b27b1904c
wangkofnew <wangkofend@163.com>
2019-08-26 13:38:33 +0800

    first commit

---
+++ src/main/java/com/example/demo/web/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.example.demo.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+    @RequestMapping(value="/hello",method= RequestMethod.GET)
+    public String sayHello(){
+        return "ok";
+    }
+}
